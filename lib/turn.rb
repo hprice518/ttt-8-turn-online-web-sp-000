b8c5b34b78db39829abad93193ed8958a6155266
Harrison Price <hprice518@gmail.com>
2020-11-01 20:28:19 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+
+def turn(move) 
+  puts "Please enter 1-9:"
+end 
+
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else
+    return true  
+  end 
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  else
+    false 
+  end
+end
+
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board,input,draw = "X") 
+  board[input] = draw
+end
